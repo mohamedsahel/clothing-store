a72077986cc915ad92677e29b1b33769a9f9b162
Mohamed Sahel <contact@mohamedsahel.com>
2020-01-16 22:04:11 +0100

    add collections to shop

--- src/App.js
+++ src/App.js
@@ -3,23 +3,17 @@ import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
-import HomePage from "./pages/homepage/homepage.component.js"
+import HomePage from "./pages/homepage/homepage.component.jsx"
+import ShopPage from "./pages/shop/shop.component"
 
 
-const Hatspage = () => (
-  <div>
-    <h1>
-      Hats page
-    </h1>
-  </div>
-)
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route exact path='/Hats' component={Hatspage} />
+        <Route exact path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
