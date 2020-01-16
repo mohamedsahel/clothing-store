cbf260e3322f596d1511de943de367e9e9ab0229
Mohamed Sahel <contact@mohamedsahel.com>
2020-01-16 16:24:23 +0100

    fix

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+
 import './App.css';
 
 import HomePage from "./pages/homepage/homepage.component.js"
 
+
+const Hatspage = () => (
+  <div>
+    <h1>
+      Hats page
+    </h1>
+  </div>
+)
+
 function App() {
   return (
     <div>
-      <HomePage />
+      <Route exact path='/' component={HomePage} />
+      <Route exact path='/Hats' component={Hatspage} />
     </div>
   );
 }
