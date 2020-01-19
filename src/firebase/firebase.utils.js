dfa59e96d941425f48ef5b79e895031dfad3302d
Mohamed Sahel <contact@mohamedsahel.com>
2020-01-19 14:37:06 +0100

    include ability to store authenticated users to our firestore database

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,34 @@ const config = {
     messagingSenderId: "259919891762",
     appId: "1:259919891762:web:6eab2011331e0667269b2b",
     measurementId: "G-C8SJMVVDBC"
+  };
+
+  export const createUserProfileDocument = async (userAuth, additionalData) => {
+
+    if (!userAuth) return;
+
+    const userRef = firestore.doc(`/users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+
+    if(!snapShot.exists){
+      const { displayName, email } = userAuth;
+      const createdAt = new Date();
+
+      try{
+        await userRef.set({
+          displayName,
+          email,
+          createdAt,
+          ...additionalData
+        })
+      } catch(error) {
+        console.log(`error creating user : ${error.message}`)
+      }
+
+    }
+
+    return userRef;
   }
 
   firebase.initializeApp(config);
--- src/pages/signin-and-signup/signin-and-signup.component.jsx
+++ src/pages/signin-and-signup/signin-and-signup.component.jsx
@@ -3,10 +3,12 @@ import React from 'react';
 import './signin-and-signup.styles.scss';
 
 import SignIn from '../../components/signin/signin.component';
+import SignUp from '../../components/signup/signup.component';
 
 const SigninAndSignup = () => (
     <div className="signin-and-signup">
         <SignIn />
+        <SignUp />
     </div>
 )
 
