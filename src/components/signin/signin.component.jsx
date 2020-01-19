d34ed14b93652891f015074fdc0af74015801801
Mohamed Sahel <contact@mohamedsahel.com>
2020-01-19 15:23:45 +0100

    finishing handleSubmit method

--- src/components/signin/signin.component.jsx
+++ src/components/signin/signin.component.jsx
@@ -4,7 +4,7 @@ import './signin.styles.scss';
 
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
-import { signInWithGoogle } from "../../firebase/firebase.utils";
+import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
 
 
 
@@ -18,14 +18,23 @@ class SignIn extends React.Component {
         }
     }
 
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault();
 
-        this.setState({ email: '', password: '' })
+        const { email, password } = this.state;
+
+        try {
+            await auth.signInWithEmailAndPassword(email, password);
+            this.setState({ email: '', password: '' })
+
+        } catch(error) {
+            console.log(error);
+        }
+
     }
 
     handleChange = event => {
-        const { value, name } = event.target;
+        const { name, value } = event.target;
 
         this.setState({ [name]: value })
     }
