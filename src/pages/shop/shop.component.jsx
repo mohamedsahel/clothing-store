97bc53e1b916ad138d3e90164e63e842e87e34e7
Mohamed Sahel <contact@mohamedsahel.com>
2020-01-25 19:48:28 +0100

    remove loggin shop page match

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -4,13 +4,11 @@ import { Route } from "react-router-dom";
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 import CollectionPage from '../collection/collection.page';
 
-const ShopPage = ({match}) => {
-    console.log(match)
-    return(
+const ShopPage = ({match}) => (
     <div className="shop-page">
         <Route exact path={`${match.path}`} component={CollectionsOverview} />
         <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
     </div>
-)};
+);
 
 export default ShopPage;
